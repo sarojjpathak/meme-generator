1ecfe733dc3ccee04f60d165c7abf9cac6c821c6
Saroj Pathak <pathaksaroj333@gmail.com>
2026-01-29 21:34:37 +0545

    adding the star feature if you like a meme you can give gold star and if you dont like the meme you can give white img

--- src/form.jsx
+++ src/form.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
 import memeimg from "./memeimg.json"
+import star from "./assets/gold.png"
+import whitestar from "./assets/white.png"
 function Form(){
     const[ display , setImg] = useState("https://i.ytimg.com/vi/cuilJhxiC_g/maxresdefault.jpg");
-
+   const[rating , setRet] = useState(true)
     function imgHandler(){
        
       const randomImage = memeimg.memes[Math.floor(Math.random()*memeimg.memes.length) ].url ;
       setImg(randomImage);
+    }
+    const starurl = rating ? star : whitestar
+    function ratingHandler(){
+        setRet((prev) => !prev)
     }
    
     return(
@@ -17,7 +23,11 @@ function Form(){
         <input type="text" placeholder="Bottom Text" />
         </div>
         <button className="file-input" onClick={imgHandler} >Get a new meme image</button>
+        <div className="imcon">
+              <img  src={starurl} alt="star-img" onClick={ratingHandler} className="imstar" />
         <img src={display} className="img--onscreen" alt="image" />
+      
+        </div>
         </div>
         
         </>
